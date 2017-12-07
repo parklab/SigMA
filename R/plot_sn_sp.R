eed95b9b9a26356a7d7a8f2c3d5cdb5f2ceec0ca
Doga Gulhan <dgulhan@Dogas-MacBook-Pro.local>
2017-12-06 23:39:33 -0500

    debug

--- R/plot_sn_sp.R
+++ R/plot_sn_sp.R
@@ -1,24 +1,29 @@
+library(ggplot2)
+
 # write a function that takes two input csv files
 # and two signature names and calculates the sp an sn
 # makes a plot
 
 calc_sn_sp_matching <- function(df, signame1, signame2, matching){
-  total1 <- dim(df$truth == signame1)[[1]]
-  total2 <- dim(df$truth == signame2)[[1]]
+  df[, matching] <- as.character(df[, matching])
+  df$truth <- as.character(df$truth)
+
+  total1 <- sum(df$truth == signame1)
+  total2 <- sum(df$truth == signame2)
   tp1 <- sum(df$truth == signame1 &
                 df[,matching] == signame1)
   tp2 <- sum(df$truth == signame2 &
                 df[, matching] == signame2)
-  fp1 <- sum(merged$truth == signame2 &
+  fp1 <- sum(df$truth == signame2 &
                 df[, matching] == signame1)
-  fp2 <- sum(merged$truth == signame1 &
+  fp2 <- sum(df$truth == signame1 &
                 df[, matching] == signame2)
-    
-  sp1 <- tp1/total1
-  sn1 <- (1 - fp1)/total2
-  sp2 <- tp2/total2
-  sn2 <- (1 - fp2)/total1
-  return(list(sp1 = sp1, sn1 = sn1, sp2 = sp2, sn2 = sn2)
+
+  sn1 <- tp1/total1
+  sp1 <- (total2 - fp1)/total2
+  sn2 <- tp2/total2
+  sp2 <- (total1 - fp2)/total1
+  return(list(sp1 = sp1, sn1 = sn1, sp2 = sp2, sn2 = sn2))
 }
 
 calc_sn_sp_cut <- function(vals_true, vals_false, cutoff_low){
@@ -34,18 +39,18 @@ calc_sn_sp_cut <- function(vals_true, vals_false, cutoff_low){
   }
   return(data.frame(cutoff_low = cutoff_low,
                     sn = sensitivity,
-                    sp = specificity)
+                    sp = specificity))
 }
 
-sn_sp_vs_nsnv <- function(df, signame1, signame2, snv_ranges, matching) 
-  sp_vec1 <- rep(0, length(snv_ranges))
-  sp_vec1 <- rep(0, length(snv_ranges))
-  sn_vec2 <- rep(0, length(snv_ranges))
-  sn_vec2 <- rep(0, length(snv_ranges))
+sn_sp_vs_nsnv <- function(df, signame1, signame2, snv_ranges, matching){
+  sp_vec1 <- rep(0, length(snv_ranges) - 1)
+  sp_vec2 <- rep(0, length(snv_ranges) - 1)
+  sn_vec1 <- rep(0, length(snv_ranges) - 1)
+  sn_vec2 <- rep(0, length(snv_ranges) - 1)
 
   for(isnv in 1:(length(snv_ranges) - 1)){
-    df_this <- df[df$total_snvs >= nsnv_ranges[[isnv]] &
-                  df$total_snvs < nsnv_ranges[[isnv + 1]],]
+    df_this <- df[df$total_snvs >= snv_ranges[[isnv]] &
+                  df$total_snvs < snv_ranges[[isnv + 1]],]
     sn_sp <- calc_sn_sp_matching(df_this, signame1, signame2, matching)
     sp_vec1[[isnv]] <- sn_sp$sp1
     sp_vec2[[isnv]] <- sn_sp$sp2
@@ -56,51 +61,66 @@ sn_sp_vs_nsnv <- function(df, signame1, signame2, snv_ranges, matching)
   df1 <- data.frame(sp = sp_vec1, 
                     sn = sn_vec1, 
                     nsnv_low = snv_ranges[1:(length(snv_ranges) - 1)],
-                    nsnv_high = nsnv_high[2:length(snv_ranges)],
+                    nsnv_high = snv_ranges[2:length(snv_ranges)],
                     truth = signame1,
                     method = matching)
   df2 <- data.frame(sp = sp_vec2, 
                     sn = sn_vec2, 
                     nsnv_low = snv_ranges[1:(length(snv_ranges) - 1)],
-                    nsnv_high = nsnv_high[2:length(snv_ranges)],
+                    nsnv_high = snv_ranges[2:length(snv_ranges)],
                     truth = signame2,
                     method = matching)
   df <- rbind(df1, df2)
   return(df)
 }
 
-plot_sn_sp(file1, 
-           file2, 
-           signame1, 
-           signame2, 
-           dependence = "total_snvs", 
-           snv_ranges = c(0, 5, 10, 15, 20, 25, 
-                          30, 35, 40, 45, 50, 55, 
-                          60, 65, 70, 75, 80, 85, 
-                          90, 95, 100),
-           cutoff_low = c(0, 0.002, 0.005, 0.007, 
-                          0.01, 0.02, 0.05, 0.1, 
-                          0.15, 0.2, 0.25, 0.3, 
-                          0.35, 0.4, 0.45, 0.5, 
-                          0.55, 0.6, 0.65, 0.7, 
-                          0.75, 0.8, 0.85, 0.875, 
-                          0.9, 0.92, 0.94, 0.96, 
-                          0.98, 0.99, 0.995, 0.997),
-           with_mathing = TRUE,
-           cutoff_c = 0.15, 
-           cutoff_l = 0.25)
+plot_sn_sp <- function(file1, 
+                       file2, 
+                       signame1, 
+                       signame2, 
+                       dependence = "total_snvs", 
+                       snv_ranges = c(5, 15, 25, 35, 45, 55, 
+                                      65, 75, 85, 100),
+                       cutoff_low = c(0, 0.002, 0.005, 0.007, 
+                                      0.01, 0.02, 0.05, 0.1, 
+                                      0.15, 0.2, 0.25, 0.3, 
+                                      0.35, 0.4, 0.45, 0.5, 
+                                      0.55, 0.6, 0.65, 0.7, 
+                                      0.75, 0.8, 0.85, 0.875, 
+                                      0.9, 0.92, 0.94, 0.96, 
+                                      0.98, 0.99, 0.995, 0.997),
+                       with_matching = FALSE,
+                       do_cutoff = FALSE,
+                       cutoff_c = 0.15, 
+                       cutoff_l = 0.25)
 {
 
   input1 <- read.csv(file1)
   input2 <- read.csv(file2)
-
+  
   input1$truth <- signame1
   input2$truth <- signame2
+  if(do_cutoff){
+    input1_l <- input1[input1$max_l > cutoff_l, ]
+    input2_l <- input2[input2$max_l > cutoff_l, ]
 
+    input1_c <- input1[input1$max_c > cutoff_c, ]
+    input2_c <- input2[input2$max_c > cutoff_c, ]
+  }else{
+    input1_l <- input1
+    input2_l <- input2
+    input1_c <- input1
+    input2_c <- input2
+  }
+
+  
   merged <- rbind(input1, input2)
+  merged_l <- rbind(input1_l, input2_l)
+  merged_c <- rbind(input1_c, input2_c)
+ 
   if(dependence == "total_snvs"){
-    df_l <- sn_sp_vs_nsnv(merged, signame1, signame2, snv_ranges, 'sig_max_l')
-    df_c <- sn_sp_vs_nsnv(merged, signame1, signame2, snv_ranges, 'sig_max_c')   
+    df_l <- sn_sp_vs_nsnv(merged_l, signame1, signame2, snv_ranges, 'sig_max_l')
+    df_c <- sn_sp_vs_nsnv(merged_c, signame1, signame2, snv_ranges, 'sig_max_c')   
     df <- rbind(df_l, df_c) 
    
     plot <- ggplot(df, aes(x = 1 - sp, y = sn)) 
@@ -108,6 +128,9 @@ plot_sn_sp(file1,
     plot <- plot + facet_wrap( ~ truth)
     plot <- plot + theme_bw()
     plot <- plot + xlab('FPR') + ylab('Sensitivity')
+    ggsave(plot, file = sprintf('sn_sp_%s_%s_%s.jpg', dependence, signame1, signame2))
+
+    write.table(df, 'test.txt', row.names = F, quote = F)
   }
 
   if(dependence == "cut_off"){
@@ -150,6 +173,6 @@ plot_sn_sp(file1,
     plot <- plot + theme_bw() + xlab('relative FPR') + ylab('relative sensitivity')
     if(with_matching) plot <- plot + labs(title = sprintf('Samples matched to %s', signame1)) 
     else plot <- plot + labs('No prior matching')
-    ggsave(plot, file = sprintf('sensitivity_specificity_vs_cutoff_%s_%s_with_matching%d.jpg', signame1, signame2, with_matching)
+    ggsave(plot, file = sprintf('sensitivity_specificity_vs_cutoff_%s_%s_with_matching%d.jpg', signame1, signame2, with_matching))
   }
 }
