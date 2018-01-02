62f122809c70e0f95340b55aefc587346d73f556
Doga Gulhan <dgulhan@Dogas-MacBook-Pro.local>
2018-01-02 05:04:22 -0500

    bug fix in mc

--- R/downsample_random.R
+++ R/downsample_random.R
@@ -1,6 +1,6 @@
 #' Randomly downsample the genomes given in the rows of a
 #' dataframe 
-#'
+#
 #' @param genome_file the file with a data frame that has the 
 #' first 96 rows need to be the 96 dimensional SNV spectra
 #' @param min_snv is the lower bound of snvs to be generated
@@ -41,26 +41,30 @@ downsample_random <- function(genome_file,
   max_mutations <- max(mutations)
 
   max_index <- (max_snv - min_snv + 1)*niter
-
   mc_snvs <- matrix(0, max_index, 96)
   index <- 1
+  max_index_this <- 0
 
   for(iter in 1:niter){
+    max_index_this <- max_index_this + (max_snv - min_snv + 1)
+    mutations_tmp <- mutations 
     for(isnv in min_snv:max_snv){
-      while(sum(mc_snvs[index,]) < isnv){
+      while(sum(mc_snvs[index,]) < isnv){ 
         location <- as.integer(round(runif(1, 0.5, 96.5), 
                                digit = 0))
-        while(mutations[[location]] <= 0){
+        while(mutations_tmp[[location]] <= 0){
           location <- as.integer(round(runif(1, 0.5, 96.5), 
                               digit = 0))
         }
-
         rand_prob <- runif(1, 0, max_mutations)
-        if(rand_prob < mutations[[location]]){
-          mc_snvs[index:max_index, location] <- mc_snvs[index:max_index, location] + 1
-          index <- index + 1
-          mutations[[location]] <- mutations[[location]] - 1
-          max_mutations <- max(mutations)
+        if(rand_prob < mutations_tmp[[location]]){
+          mc_snvs[index:max_index_this, location] <- mc_snvs[index:max_index_this, location] + 1
+          if(sum(mc_snvs[index, ]) >= min_snv) index <- index + 1
+          mutations_tmp[[location]] <- mutations_tmp[[location]] - 1
+          max_mutations <- max(mutations_tmp)
+        }
+        if(index > max_index_this){
+          break
         }
       }
     }
