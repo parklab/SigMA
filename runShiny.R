304c3e50be6cce31c52354c75a542a3333d224d1
Scott Ouellette <scottx611x@gmail.com>
2018-12-03 21:09:03 -0500

    Move existing app into its own app context directory

--- runShiny.R
+++
@@ -1,8 +0,0 @@
-devtools::load_all()
-library('shiny')
-runApp('shiny')
-
-# Then insert the full path of the inst/extdata/ directory in the package
-# choose Breast Cancer 
-# click run
-# click at one of the tags
