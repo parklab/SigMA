3098cf84b7a1e71ae765233c74657aa2773ced9d
Scott Ouellette <scottx611x@gmail.com>
2018-12-03 21:10:08 -0500

    Add script to build and run image

---
+++ build_image.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+REPO=scottx611x/sigma
+
+export STAMP=`date +"%Y-%m-%d_%H-%M-%S"`
+export SUFFIX=-standalone
+echo "image-$STAMP"
+
+CONTAINER_NAME="container-$STAMP$SUFFIX"
+mkdir "/tmp/$CONTAINER_NAME"
+
+docker pull $REPO
+docker build --cache-from $REPO:latest \
+             --tag image-$STAMP \
+             .
+docker run --name $CONTAINER_NAME \
+           --detach \
+           -p 3242:3242 \
+           image-$STAMP
+rm -r "/tmp/$CONTAINER_NAME"
