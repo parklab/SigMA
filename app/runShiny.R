e41ab408a91b31bae53a745fe1b1b445d08c6154
Scott Ouellette <scottx611x@gmail.com>
2018-12-03 21:09:42 -0500

    Expose runShiny.R through 0.0.0.0 and specify new app location in container

---
+++ app/runShiny.R
@@ -0,0 +1,8 @@
+devtools::load_all()
+library('shiny')
+runApp('/srv/shinyapps/app/shiny/', 3242, host='0.0.0.0')
+
+# Then insert the full path of the inst/extdata/ directory in the package
+# choose Breast Cancer 
+# click run
+# click at one of the tags
