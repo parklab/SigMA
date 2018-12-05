8cfbce0d2964d05f9312f81f7da474892b1fdf07
Scott Ouellette <scottx611x@gmail.com>
2018-12-05 13:15:40 -0500

    Move docker specific files around

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -11,11 +11,13 @@ CONTAINER_NAME="container-$STAMP$SUFFIX"
 mkdir "/tmp/$CONTAINER_NAME"
 
 docker pull $REPO
-docker build --cache-from $REPO:latest \
+
+docker build -f docker-context/Dockerfile \
+             --cache-from $REPO:latest \
              --tag image-$STAMP \
              .
-docker run --name $CONTAINER_NAME \
-           --detach \
-           -p 3242:3242 \
-           image-$STAMP
+# docker run --name $CONTAINER_NAME \
+#            --detach \
+#            -p 3242:3242 \
+#            image-$STAMP
 rm -r "/tmp/$CONTAINER_NAME"
