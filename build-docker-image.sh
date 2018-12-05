4a92fc308767cb05f5e10e05c6ea855d050e8477
Scott Ouellette <scottx611x@gmail.com>
2018-12-05 13:26:09 -0500

    Update build-docker-image script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -4,11 +4,7 @@ set -e
 REPO=scottx611x/sigma
 
 export STAMP=`date +"%Y-%m-%d_%H-%M-%S"`
-export SUFFIX=-standalone
 echo "image-$STAMP"
-
-CONTAINER_NAME="container-$STAMP$SUFFIX"
-mkdir "/tmp/$CONTAINER_NAME"
 
 docker pull $REPO
 
@@ -16,8 +12,3 @@ docker build -f docker-context/Dockerfile \
              --cache-from $REPO:latest \
              --tag image-$STAMP \
              .
-# docker run --name $CONTAINER_NAME \
-#            --detach \
-#            -p 3242:3242 \
-#            image-$STAMP
-rm -r "/tmp/$CONTAINER_NAME"
