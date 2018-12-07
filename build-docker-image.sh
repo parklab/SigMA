62407ac2f42c2f75d10a080815e3b4f69c78c362
Scott Ouellette <scottx611x@gmail.com>
2018-12-07 11:16:12 -0500

    Add useful comments to docker image building script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -9,10 +9,17 @@ echo "$DOCKER_IMAGE_TAG"
 
 docker pull $REPO
 
-docker build -f docker-context/Dockerfile \
-             --cache-from rocker/shiny:3.5.0,$REPO:latest \
-             --tag image-$STAMP \
-             .
+if [[ -v CONTINUOUS_INTEGRATION ]]; then 
+    # Speed up CI build times with docker caching workaround. See: https://github.com/moby/moby/issues/20316#issuecomment-358260810
+    docker build \
+        -f docker-context/Dockerfile \
+        --cache-from rocker/shiny:3.5.0,$REPO:latest \
+        --tag image-$STAMP \
+        .;
 
-# Run a container in CI so that we can grab the tag from the docker cli
-if [[ -v CONTINUOUS_INTEGRATION ]]; then docker run -d -p 3242:3242 $DOCKER_IMAGE_TAG; fi
+    # Run a container in CI so that we can grab its tag from the docker cli
+    docker run -d -p 3242:3242 $DOCKER_IMAGE_TAG; 
+else
+    # If building locally, just use cache relative to the local machine
+    docker build -f docker-context/Dockerfile .;
+fi
