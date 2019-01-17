2b300001b40d1a527b6bde515625521ed7119777
Scott Ouellette <scottx611x@gmail.com>
2019-01-17 16:15:35 -0500

    Add `devtools::load_all()` to server.R

--- shiny/server.R
+++ shiny/server.R
@@ -1,6 +1,8 @@
 library('DT')
 library(shinycssloaders)
 
+devtools::load_all(path = "../R")
+
 myplot <- function(x) {
   plot <- gridExtra::arrangeGrob(x)
   class(plot) <- c("myplot", class(plot))
