07f219f3c24994f43285e1ca450ffefbe4c1ab48
Doga Gulhan <dcgulhan@gmail.com>
2020-04-14 01:01:32 +0200

    update install.R

--- install.R
+++ install.R
@@ -1,17 +1,27 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install(c("BSgenome",
-"BSgenome.Hsapiens.UCSC.hg19",
-"devtools",
-"DT",
-"GenomicRanges",
-"ggplot2",
-"gbm",
-"gridExtra",
-"IRanges",
-"nnls",
-"pheatmap",
-"reshape2",
-"shinyccsloaders",
-"Rmisc",
-"VariantAnnotation"))
+packages <- c("BSgenome",
+    "BSgenome.Hsapiens.UCSC.hg19",
+    "devtools",
+    "DT",
+    "GenomicRanges",
+    "ggplot2",
+    "gbm",
+    "gridExtra",
+    "IRanges",
+    "nnls",
+    "pheatmap",
+    "reshape2",
+    "shinyccsloaders",
+    "Rmisc",
+    "VariantAnnotation")
+
+if(getRversion() > "3.6.0"){
+  if(grepl('3.6', getRversion())){
+    if (!requireNamespace("BiocManager", quietly = TRUE))
+      install.packages("BiocManager")
+    BiocManager::install(packages)
+  }
+}
+if(getRversion() < "3.6.0" & getRversion() >= "3.5.0"){
+  source("https://bioconductor.org/biocLite.R")
+  biocLite(packages)
+}
