d7b6627b464e1fc5a017981e1019e89543c2de71
Doga Gulhan <dcgulhan@gmail.com>
2022-03-13 00:36:33 +0100

    example script

--- examples/test_estimate_exposures_by_matching_to_WGS_simul.R
+++ examples/test_estimate_exposures_by_matching_to_WGS_simul.R
@@ -1,13 +1,20 @@
 devtools::load_all()
 
-
+# this script demonstrates different types of NNLS calculations one can do with SigMA
+# using simulated data, which allows comparison to the calculation in WGS data
+#
+# you can do the same calculations with actual panel data you would only miss the part
+# where calculations in panel are compared to WGS
+#
+# please run the following script after test_tune_example_forPfizer.R script run use the following
+# which creates the simulated data used below
 tumor_type <- 'breast'
 catalog_name <- 'cosmic_v3p2_inhouse'
-check_msi <- T
-data <- 'msk'
+check_msi <- F # set T if you might have msi samples
+data <- 'fo'
 maf_percent <- NULL # replace with 0.001 for panels without matched normal
 
-genome_file <- 'matrix_96dim_msk_breast_nomsisimulation.csv'
+genome_file <- 'matrix_96dim_fo_breast_nomsisimulation.csv'
 df_genome <- read.csv(genome_file)
 
 ## use the lines below if you have not first tried the test_tune_example_forPfizer.R script
@@ -15,7 +22,6 @@ outfile <- run(genome_file = genome_file, readjust = T,
                tumor_type = tumor_type, data = data, check_msi = F,
                catalog_name = catalog_name)
 
-## if you have already added the specific gbm model for your data
 outfile <- run(genome_file,
                       data = paste0('gbm_for_', tumor_type,'_', data, '_', catalog_name, '_maf_percent_', maf_percent),
                       tumor_type = tumor_type,
@@ -25,7 +31,6 @@ outfile <- run(genome_file,
                       check_msi = check_msi,
                       custom = T,
                       norm96 = weight_3Nfreq$fo)
-		    
 df_sigma_out <- read.csv(outfile)
 df_sigma_out <- llh_max_characteristics(df_sigma_out,tumor_type = tumor_type, catalog_name = catalog_name)
 df_sigma_lite <- lite_df(df_sigma_out)
@@ -50,12 +55,10 @@ df <- llh_max_characteristics_wgs_data(df_likelihoods,
                                        tumor_type = tumor_type,
 				       catalog_name = catalog_name,
 				       check_msi = check_msi)
-
 df <- df[,colnames(df) %in% c('sig_max_ml','max_ml','is_sig3_sig_max','Signature_3_ml_wgs_data', 'max_ml_sample_sigs_all', 'max_ml_sample_exps_all', 'is_msi_sig_max', 'is_pole_sig_max') | grepl('ave_ml_', colnames(df))]
 
 df_exps_sample_max <- get_sig_exps(df, col_exps = 'max_ml_sample_exps_all', col_sigs = 'max_ml_sample_sigs_all')
 colnames(df_exps_sample_max) <- paste0('max_ml_sample_', colnames(df_exps_sample_max))
-
 inds <- match(df_genome$tumor, df_sigma_lite$tumor)
 df_out <- cbind(df_genome,
                 df_sigma_lite[inds,!(colnames(df_sigma_lite) %in% c('tumor', 'exp_sig3','tumor'))],
@@ -65,6 +68,8 @@ df_out <- cbind(df_genome,
 		df_exps_sample_max[inds,])
 
 df_out$rat_sig3 <- df_out$exp_sig3/df_out$total_snvs
+
+# up to here the same script can be used for data the lines following this are comparisons to WGS calculation
 
 # the is_sig3 tag comes from the WGS analysis while the rest of the exposures are calculated from panel simulations
 #
@@ -104,7 +109,6 @@ df_wgs_is_sig3 <- get_Sig3_llh_from_wgs_data(colnames(df_wgs), tumor_type = tumo
 
 df_exps_wgs <- data.frame(matrix(0, dim(df_wgs)[[2]], length(signatures)))
 colnames(df_exps_wgs) <- gsub(signatures, pattern = 'Signature_', replace = 'exp_sig')
-
 for(i in 1:dim(df_wgs)[[2]]){
   signatures_this <- signatures
   spec <- df_wgs[,i]
@@ -117,11 +121,10 @@ for(i in 1:dim(df_wgs)[[2]]){
       signatures_this <- signatures_this[!(signatures_this %in% c(signames_per_tissue_per_catalog[[catalog_name]][['msi']], signames_per_tissue_per_catalog[[catalog_name]][['msi_extra']], signames_per_tissue_per_catalog[[catalog_name]][['pole']]))]
   }
   
-  
   df_decompose <- match_to_catalog(data.frame(t(df_wgs))[i,], cosmic_catalog[,signatures_this], data = 'wgs', method = 'decompose')
   df_decompose <- cbind(df_decompose, get_sig_exps(df_decompose, col_sigs='sigs_all', col_exps = 'exps_all'))
   for(sig in signatures_this){
-    df_decompose[which(df_decompose[,paste0(sig, '_l_rat')] < 0.5), gsub(sig, pattern = 'Signature_', replace = 'exp_sig')]  <- 0
+    df_decompose[which(df_decompose[,paste0(sig, '_l_rat')] < 0.505), gsub(sig, pattern = 'Signature_', replace = 'exp_sig')]  <- 0
   }
   paste0(gsub(signatures_this, pattern = 'Signature_', replace = 'exp_sig'))
   df_exps_wgs[i, gsub(signatures_this, pattern = 'Signature_', replace = 'exp_sig')] <- df_decompose[,gsub(signatures_this, pattern = 'Signature_', replace = 'exp_sig')]
@@ -132,30 +135,37 @@ df_out <- cbind(df_out, df_exps_wgs[match(df_out$tumor, colnames(df_wgs)),])
 # an example comparing wgs exposure in the same sample wgs_exp_sig13 to the one calculated in panel
 # note that for clustered mutational processes such as APOBEC (e.g. Sig2, Sig13) a subset of genomic
 # regions can have higher exposure than the whole genome if a cluster of mutations are captured
-plot_nnls_panel <- ggplot2::ggplot(df_out, ggplot2::aes(x = wgs_exp_sig13, y = exp_sig13/total_snvs))
+sig_selected <- 'exp_sig17b' # change this to see other signatures of interest
+
+df_out$exp_sig_selected <- df_out[,sig_selected]
+df_out$wgs_exp_sig_selected <- df_out[,paste0('wgs_', sig_selected)]
+df_out$cluster_exp_sig_selected <- df_out[,paste0('cluster_', sig_selected)]
+
+plot_nnls_panel <- ggplot2::ggplot(df_out, ggplot2::aes(x = wgs_exp_sig_selected, y = exp_sig_selected/total_snvs))
 plot_nnls_panel <- plot_nnls_panel + ggplot2::geom_point(size = 0.5, alpha = 0.5)
 plot_nnls_panel <- plot_nnls_panel + ggplot2::geom_abline(slope = 1)
 plot_nnls_panel <- plot_nnls_panel + ggplot2::geom_smooth(method = 'lm')
 plot_nnls_panel <- plot_nnls_panel + ggplot2::theme_bw()
-ggplot2::ggsave(plot_nnls_panel, file = 'Sig13_scatter_nnls_panel_vs_WGS.pdf', width = 2, height = 2)
+ggplot2::ggsave(plot_nnls_panel, file = paste0(sig_selected, '_scatter_nnls_panel_vs_WGS.pdf'), width = 2, height = 2)
 
 # you can try cleaning out assignments with potential overfitting problem
-df_out$Signature_13_l_rat <- df_sigma_out$Signature_13_l_rat[match(df_out$tumor, df_sigma_out$tumor)]
-df_out$exp_sig13_cleaned <- df_out$exp_sig13
-df_out$exp_sig13_cleaned[df_out$Signature_13_l_rat < 0.6] <- 0
-plot_nnls_panel_clean <- ggplot2::ggplot(df_out, ggplot2::aes(x = wgs_exp_sig13, y = exp_sig13_cleaned/total_snvs))
+df_out$selected_l_rat <- df_sigma_out[match(df_out$tumor, df_sigma_out$tumor), paste0(gsub(sig_selected, pattern = 'exp_sig', replace = 'Signature_'), '_l_rat')] 
+df_out$exp_sig_selected_cleaned <- df_out$exp_sig_selected
+df_out$exp_sig_selected_cleaned[df_out$selected_l_rat < 0.6] <- 0
+
+plot_nnls_panel_clean <- ggplot2::ggplot(df_out, ggplot2::aes(x = wgs_exp_sig_selected, y = exp_sig_selected_cleaned/total_snvs))
 plot_nnls_panel_clean <- plot_nnls_panel_clean + ggplot2::geom_point(size = 0.5 , alpha = 0.5)
 plot_nnls_panel_clean <- plot_nnls_panel_clean + ggplot2::geom_abline(slope = 1)
 plot_nnls_panel_clean <- plot_nnls_panel_clean + ggplot2::geom_smooth(method = 'lm')
 plot_nnls_panel_clean <- plot_nnls_panel_clean + ggplot2::theme_bw()
-ggplot2::ggsave(plot_nnls_panel_clean, file = 'Sig13_scatter_nnls_panel_cleaned_vs_WGS.pdf', width = 2, height = 2)
+ggplot2::ggsave(plot_nnls_panel_clean, file = paste0(sig_selected, '_scatter_nnls_panel_cleaned_vs_WGS.pdf'), width = 2, height = 2)
 
 # you can also compare to the matched cluster
-plot_nnls_cluster <- ggplot2::ggplot(df_out, ggplot2::aes(x = wgs_exp_sig13, y = cluster_exp_sig13))
+plot_nnls_cluster <- ggplot2::ggplot(df_out, ggplot2::aes(x = wgs_exp_sig_selected, y = cluster_exp_sig_selected))
 plot_nnls_cluster <- plot_nnls_cluster + ggplot2::geom_point(size =0.5, alpha = 0.5)
 plot_nnls_cluster <- plot_nnls_cluster + ggplot2::geom_abline(slope = 1)
 plot_nnls_cluster <- plot_nnls_cluster + ggplot2::geom_smooth(method = 'lm')
 plot_nnls_cluster <- plot_nnls_cluster + ggplot2::theme_bw()
-ggplot2::ggsave(plot_nnls_cluster, file = 'Sig13_scatter_nnls_matched_cluster_WGS_vs_WGS.pdf', width = 2, height = 2)
+ggplot2::ggsave(plot_nnls_cluster, file = paste0(sig_selected, '_scatter_nnls_matched_cluster_WGS_vs_WGS.pdf'), width = 2, height = 2)
 
 # For the signatures you are interested please compare all the approaches above to decide which gives more accurate estimates from panel
