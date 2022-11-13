f3af5a1065ed182af92a03e7606bf15c73870292
Doga C. Gulhan <dcgulhan@gmail.com>
2022-11-14 00:13:50 +0100

    making catalog_name NULL in run so that it is explicitly set according to use case

--- examples/run_SigMA_MMRD.R
+++ examples/run_SigMA_MMRD.R
@@ -8,7 +8,8 @@ output_SigMA <- run(genome_file = 'example_mmrd_SBS_matrix.csv',
                      data = 'tcga_mc3',
                      tumor_type = 'crc', 
                      do_mva = F, # can also be set to T for Sig3 but not necessary for MMRD detection
-                     check_msi = T)
+                     check_msi = T,
+		     catalog_name = 'cosmic_v3_inhouse')
 predict_mmrd(input_file = output_SigMA,
              data = 'tcga_mc3')
 
